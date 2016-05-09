92691017e97085392bfa7b1f4da78caf23dedf58
Jake Obron <jbobron@gmail.com>
2016-05-09 14:18:53 -0700

    changes / response again

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/dist'));
@@ -15,7 +16,8 @@ app.use(express.static(__dirname + '/dist'));
 
 // set the home page route
 app.get('/', function(req, res) {
-  res.send("hello world")
+  // res.send("hello world")
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
   // ejs render automatically looks in the views folder
   // res.render('index');
 
